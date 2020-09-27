a92fd92b22aa0710249e884e62860353f1c03309
Girish V <girish.v@needl.ai>
2020-09-27 20:27:01 +0530

    simple swiper

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _ctrler = PageController(viewportFraction: 0.8);
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _ctrler.addListener(() {
+      final next = _ctrler.page.round();
+      if (_currentPage != next)
+        setState(() {
+          _currentPage = next;
+        });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(),
+      body: PageView(
+        controller: _ctrler,
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: _currentPage == 0 ? 100 : 200,
+              right: 20,
+              left: 20,
+            ),
+            color: Colors.amber,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: _currentPage == 1 ? 100 : 200,
+              right: 20,
+              left: 20,
+            ),
+            color: Colors.red,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: _currentPage == 2 ? 100 : 200,
+              right: 20,
+              left: 20,
+            ),
+            color: Colors.purple,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: _currentPage == 3 ? 100 : 200,
+              right: 20,
+              left: 20,
+            ),
+            color: Colors.green,
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: _currentPage == 4 ? 100 : 200,
+              right: 20,
+              left: 20,
+            ),
+            color: Colors.amber,
+          ),
+        ],
       ),
     );
   }
